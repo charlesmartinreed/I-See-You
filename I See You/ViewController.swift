22e6f1e6838c55aa925b0966ef92b4f382c93089
charlesmartinreed <charlesmartinreed@icloud.com>
2019-01-28 21:18:54 -0600

    Add VNRequestHandler for face observation.

--- I See You/ViewController.swift
+++ I See You/ViewController.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
+    var imageView: UIImageView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //load test image
+        guard let image = UIImage(named: "testPicture") else { return }
+        imageView?.image = image
+        
+        guard let cgImage = image.cgImage else { return }
+        
+        //let faceDetect = VNDetectFaceRectanglesRequest - detects the rough position of face in a photo
+        //let faceDetailsDetect = VNDetectFaceLandmarksRequest - detects facial aspects like eyes, nose. Recommended to handle this asynchronously and then push it back to the main thread.
+        
+        let request = VNDetectFaceLandmarksRequest { [weak self] request, error in
+            if let observations = request.results as? [VNFaceObservation] {
+                DispatchQueue.main.async {
+                    //render the result
+                }
+        } else {
+                print(error?.localizedDescription ?? "No observations available")
+            }
+        }
+        //prepare to run request
+        let handler = VNImageRequestHandler(cgImage: cgImage)
+        
+        DispatchQueue.global().async {
+            
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+
+    }
+    
+    override func loadView() {
+        //this image view is our view
+        imageView = UIImageView()
+        imageView?.contentMode = .scaleAspectFit
+        view = imageView
     }
 
 
